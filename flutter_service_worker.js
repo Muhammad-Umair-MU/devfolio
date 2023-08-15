b87f22afa03dbf0732d87fc813756b66f8aec0ed
Muhammad Umair Khan <umair.ims19@gmail.com>
2023-08-15 22:02:10 +0500

    u

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -85,8 +85,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "a20342d2e0a0046a509f7063bcb66014
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a034e3322865f38689026069e3ba1d59",
-"/": "a034e3322865f38689026069e3ba1d59",
+"index.html": "12c9d8c1c5612561a87c1441e1e6822e",
+"/": "12c9d8c1c5612561a87c1441e1e6822e",
 "main.dart.js": "5cbcc5e240888e14b9d81e1af512db2a",
 "manifest.json": "88e2c1e2e34b25a4ba73188b19188c63",
 "version.json": "ac7d57fe31a91f73b206e297d700b7c5"};
